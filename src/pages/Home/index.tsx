a5b3133930da24f61e7e08ec5c60ed92448c8755
Samuellima99 <lima300799@gmail.com>
2023-09-20 20:50:17 -0300

    refactor: ajustes

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 import image01 from '../../assets/images/cyber01.jpeg';
 import image02 from '../../assets/images/cyber02.jpeg';
@@ -8,49 +8,50 @@ import image05 from '../../assets/images/cyber05.jpeg';
 
 import * as S from './styles';
 
+const items = [
+  {
+    id: 1,
+    name: 'Nyx',
+    avatar: image01,
+    active: true,
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vitae mattis ante. In ut risus at quam congue vehicula et at nulla. Etiam interdum felis mollis augue euismod imperdiet. Donec ac posuere enim.',
+  },
+  {
+    id: 2,
+    name: 'Gaia',
+    avatar: image02,
+    active: false,
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vitae mattis ante. In ut risus at quam congue vehicula et at nulla. Etiam interdum felis mollis augue euismod imperdiet. Donec ac posuere enim.',
+  },
+  {
+    id: 3,
+    name: 'Proton',
+    avatar: image03,
+    active: false,
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vitae mattis ante. In ut risus at quam congue vehicula et at nulla. Etiam interdum felis mollis augue euismod imperdiet. Donec ac posuere enim.',
+  },
+  {
+    id: 4,
+    name: 'Electra',
+    avatar: image04,
+    active: false,
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vitae mattis ante. In ut risus at quam congue vehicula et at nulla. Etiam interdum felis mollis augue euismod imperdiet. Donec ac posuere enim.',
+  },
+  {
+    id: 5,
+    name: 'Orix',
+    avatar: image05,
+    active: false,
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vitae mattis ante. In ut risus at quam congue vehicula et at nulla. Etiam interdum felis mollis augue euismod imperdiet. Donec ac posuere enim.',
+  },
+];
+
 export function Home() {
-  const [items, setItems] = useState([
-    {
-      id: 1,
-      name: 'Nyx',
-      avatar: image01,
-      active: true,
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vitae mattis ante. In ut risus at quam congue vehicula et at nulla. Etiam interdum felis mollis augue euismod imperdiet. Donec ac posuere enim.',
-    },
-    {
-      id: 2,
-      name: 'Gaia',
-      avatar: image02,
-      active: false,
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vitae mattis ante. In ut risus at quam congue vehicula et at nulla. Etiam interdum felis mollis augue euismod imperdiet. Donec ac posuere enim.',
-    },
-    {
-      id: 3,
-      name: 'Proton',
-      avatar: image03,
-      active: false,
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vitae mattis ante. In ut risus at quam congue vehicula et at nulla. Etiam interdum felis mollis augue euismod imperdiet. Donec ac posuere enim.',
-    },
-    {
-      id: 4,
-      name: 'Electra',
-      avatar: image04,
-      active: false,
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vitae mattis ante. In ut risus at quam congue vehicula et at nulla. Etiam interdum felis mollis augue euismod imperdiet. Donec ac posuere enim.',
-    },
-    {
-      id: 5,
-      name: 'Orix',
-      avatar: image05,
-      active: false,
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vitae mattis ante. In ut risus at quam congue vehicula et at nulla. Etiam interdum felis mollis augue euismod imperdiet. Donec ac posuere enim.',
-    },
-  ]);
   const [activeItem, setActiveItem] = useState(1);
 
   function handleActiveAccordion(itemId: number) {
